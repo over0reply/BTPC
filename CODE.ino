6fdf453260b9830f70a065e76e2a226f1dcdf28f
erfan <167025066+over0reply@users.noreply.github.com>
2024-07-07 22:54:10 +0330

    Update CODE.ino

--- CODE.ino
+++ CODE.ino
@@ -1,40 +1,19 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include <SoftwareSerial.h>
 
-#define RST_PIN 9 // Configurable, see typical pin layout above
-#define SS_PIN 10 // Configurable, see typical pin layout above
-
-MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance
+SoftwareSerial BTSerial(10, 11); // RX, TX for Bluetooth
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communications with the PC
-  SPI.begin();        // Init SPI bus
-  rfid.PCD_Init();    // Init MFRC522
-  Serial.println("Scan an RFID card...");
+  Serial.begin(9600);   // Communication with PC
+  BTSerial.begin(9600); // Communication with Bluetooth module
 }
 
 void loop() {
-  // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent()) {
-    return;
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    BTSerial.println(command);  // Send command to Bluetooth module
   }
-
-  // Select one of the cards
-  if ( ! rfid.PICC_ReadCardSerial()) {
-    return;
+  if (BTSerial.available()) {
+    String response = BTSerial.readStringUntil('\n');
+    Serial.println(response);   // Send response to PC
   }
-
-  // Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content = "";
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(rfid.uid.uidByte[i], HEX);
-    content.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(rfid.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  
-  delay(1000); // Add a delay to prevent flooding the serial output
-  rfid.PICC_HaltA(); // Halt PICC
 }
